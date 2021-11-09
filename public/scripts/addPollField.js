e2d3215b314168ae772ac7bd7fc1373bb5c246df
Andradev101 <luizquot@gmail.com>
2021-11-08 22:02:22 -0300

    db is now adding and updating keys as inteded, but still some work to be done

--- public/scripts/addPollField.js
+++ public/scripts/addPollField.js
@@ -8,7 +8,7 @@ function createNewField(){
     `
     <div id="opt${i++}" class="inputBlock pollOpt">
         <label>Poll Option #${i}</label>
-        <input id="pollTitle">
+        <input type="text">
     </div>
     `
     pollOpt.insertAdjacentHTML("beforeend", element);
--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,7 +2,7 @@ console.log("aaaa");
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.3.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.3.0/firebase-firestore.js";
-import { collection, addDoc, doc, setDoc, getDocs } from "https://www.gstatic.com/firebasejs/9.3.0/firebase-firestore.js"; 
+import { collection, addDoc, doc, setDoc, getDocs, updateDoc, getDoc } from "https://www.gstatic.com/firebasejs/9.3.0/firebase-firestore.js"; 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -25,6 +25,7 @@ const db = getFirestore();
 const fName = document.querySelector("#firstName");
 const lName = document.querySelector("#lastName");
 const pollTitle = document.querySelector("#pollTitle");
+const pollOpts = document.querySelector("body > div.createPoll > div.pollOpt")
 //btns
 const newUserAddBtn = document.querySelector("body > div.newUser > button");
 const checkIdBtn = document.querySelector("#createPollBtn")
@@ -39,8 +40,14 @@ async function createUser(){
     alert(`Copy this ID: ${docRef.id}`);
   } catch (e) {
     console.error("Error adding document: ", e);
-  }
+  } 
 }
+
+// async function readOneDoc(){
+//   const snappy = await doc(collection(db, "users", "batata"))
+//   console.log(snappy.exists());
+// }
+// readOneDoc()
 
 //const userRef = collection(db,"users");
 // db.collection("users").get().then((snapshot) => {
@@ -65,27 +72,53 @@ async function createUser(){
   });
 */
 
+// //THIS WOOORKS
+//   //SEARCH DOC SHOW IF EXISTS
+//   const docRef = doc(db, "users", "6rCpdQbrmA3XY09RmZMe")
+//   const docSnap = await getDoc(docRef);
+//   if (docSnap.exists()) {
+//     console.log("Document data:", docSnap.data());
+//   } else {
+//     console.log("No such document!");
+//   }
+// //UPDATE DOC
+// const userRef = docRef;
+// await updateDoc(userRef, {
+//   pollTitle: "cudecaramujo",
+//   batata: 123
+// });
+
 //CREATE POLL
 async function createPoll(){
   let userUniqueKey = idNewPoll.value;
+  let options = await getPollOpts(); 
   try {
-    const querySnapshot = await getDocs(collection(db, "users"));
-    querySnapshot.forEach((doc) => { 
-      if(userUniqueKey == doc.id){
-        console.log("doc found: "+ doc.id +" => "+ JSON.stringify(doc.data()));
-      }
-    });
+    const userRef = doc(db, "users", `${userUniqueKey}`);
+    const getSnap = await getDoc(userRef, { merge: true })
+    console.log(getSnap.data());
 
-    const docRef = await addDoc(collection(db, "users",`${userUniqueKey}`), {
-      pollTitle: pollTitle.value
-    });
-    
+    await updateDoc(userRef, {
+      polls:[ 
+              {
+                pollTitle: pollTitle.value,
+                opts: options
+              }
+            ]
+    })
   } catch (e) {
     console.error("Error adding document: ", e);
   }
-
 }
-
-//FUNCTIONS
+//GET POLL OPTIONS
+async function getPollOpts(){
+  let pollOptArr=[];
+  for (let i = 0; i < pollOpts.childElementCount; i++) {
+    let opt = document.querySelector(`#opt${i}`);
+    pollOptArr.push(opt.children[1].value);
+  }
+  console.log(pollOptArr);
+  return pollOptArr
+}
+//EVENT LISTENERS
 newUserAddBtn.addEventListener("click", createUser);
 checkIdBtn.addEventListener("click", createPoll);
